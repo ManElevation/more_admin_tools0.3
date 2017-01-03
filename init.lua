c1e1bc8646abd785c057217b8a5299729a50a825
ManElevation <morenavaj@gmail.com>
2017-01-03 12:41:18 +1000

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,10 @@
+dofile(modpath .. "/adminpic.lua")
+dofile(modpath .. "/admininv.lua")
+dofile(modpath .. "/adminblock.lua")
+dofile(modpath .. "/adminlump.lua")
+dofile(modpath .. "/adminingot.lua")
+dofile(modpath .. "/adminmine.lua")
+
+if minetest.setting_getbool("log_mods") then
+	minetest.log("action", S("More Admin Stuff loaded!"))
+end
